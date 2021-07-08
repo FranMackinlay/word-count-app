dfe82bdf63422cdbe8a1b4d9919e5813c4242e6a
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-08 17:00:08 +0200

    IMPROVE, word count component to support dynamic sorting

--- src/components/WordCountComponent.tsx
+++ src/components/WordCountComponent.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import TableComponent from './TableComponent';
 import WordListCount from '../interfaces/interfaces';
+import ByOrder from '../interfaces/interfaces';
 import LoadingBox from './LoadingBox';
 import styles from '../styles/wordCountComponent.module.css';
 
@@ -11,6 +12,7 @@ export default function WordCountComponent() {
   const [userInput, setUserInput] = useState<string>('');
   const [wordsList, setWordsList] = useState<string[]>([]);
   const [wordsListCount, setWordsListCount] = useState<WordListCount[]>([]);
+  const [sortedWordsList, setSortedWordsList] = useState<WordListCount[]>([]);
 
 
   const onUserInput = (e: { target: { value: string; }; }) => {
@@ -18,20 +20,33 @@ export default function WordCountComponent() {
     setUserInput(e.target.value);
   }
 
-  function getOccurrence(array: string[], value: string) {
+  const getOccurrence = (array: string[], value: string): number => {
     return array.filter((v: string) => (v === value)).length;
   }
 
+  const sortData = (orderBy: ByOrder): void => {
+    setTimeout(() => {
+      let sortedArray: WordListCount[] = wordsListCount.sort((obj1: WordListCount, obj2: WordListCount) => orderBy.order === 'asc' ? obj1[orderBy.by] < obj2[orderBy.by] ? -1 : 1 : obj1[orderBy.by] > obj2[orderBy.by] ? -1 : 1);
+      setSortedWordsList(sortedArray);
+    })
+  }
+
   useEffect(() => {
-    const wordsArray = userInput.split(' ');
+    setWordsListCount(sortedWordsList);
+  }, [sortedWordsList])
+
+
+  useEffect(() => {
+    const wordsArray = userInput.replace(/[^a-zA-Z ]/g, '').toLowerCase().split(' ');
     setWordsList(wordsArray);
   }, [userInput]);
 
   useEffect(() => {
     let wordListCountTmp: WordListCount[] = [];
 
-    wordsList.forEach((word: string) => {
+    wordsList.forEach((word: string, index: number) => {
       wordListCountTmp.push({
+        id: index,
         word,
         count: getOccurrence(wordsList, word)
       });
@@ -39,9 +54,11 @@ export default function WordCountComponent() {
 
     const wordListCountMap = new Map(wordListCountTmp.map((item: WordListCount) => [item.word, item])).values();
 
-    wordListCountTmp = Array.from(wordListCountMap, ({ word, count }) => ({ word, count }));
+    wordListCountTmp = Array.from(wordListCountMap, ({ id, word, count }) => ({ id, word, count }));
 
     wordListCountTmp = wordListCountTmp.filter((item: WordListCount) => item.word !== '');
+
+    wordListCountTmp = wordListCountTmp.map((item: WordListCount, index: number) => ({ id: index, word: item.word, count: item.count }))
 
     setWordsListCount(wordListCountTmp);
     setTimeout(() => setLoading(false), 1000)
@@ -53,11 +70,11 @@ export default function WordCountComponent() {
         <textarea rows={Math.round(wordsList.length / 8)} className="form-control" id="floatingTextarea" onChange={onUserInput}></textarea>
       </div>
       <div className="words-table col-md-6">
-        {wordsListCount.length && !loading ? (
-          <TableComponent wordsListCount={wordsListCount}></TableComponent>
+        {loading ? (
+          <LoadingBox></LoadingBox>
         ) :
-          loading ? (
-            <LoadingBox></LoadingBox>
+          wordsListCount.length ? (
+            <TableComponent wordsListCount={wordsListCount} sortData={sortData}></TableComponent>
           ) : (
             <p>Please start typing for results</p>
           )}

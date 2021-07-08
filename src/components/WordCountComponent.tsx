84460a09fa0ce9c2b2026e8a726a02a8e8380151
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-08 12:11:37 +0200

    ADD, word count component loading box and styles

--- src/components/WordCountComponent.tsx
+++ src/components/WordCountComponent.tsx
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react'
 import TableComponent from './TableComponent';
+import WordListCount from '../interfaces/interfaces';
+import LoadingBox from './LoadingBox';
+import styles from '../styles/wordCountComponent.module.css';
 
-
-interface WordListCount {
-  word: string
-  count: number
-}
 
 export default function WordCountComponent() {
 
+  const [loading, setLoading] = useState<boolean>(false);
   const [userInput, setUserInput] = useState<string>('');
   const [wordsList, setWordsList] = useState<string[]>([]);
   const [wordsListCount, setWordsListCount] = useState<WordListCount[]>([]);
 
 
   const onUserInput = (e: { target: { value: string; }; }) => {
+    setLoading(true);
     setUserInput(e.target.value);
   }
 
@@ -44,18 +44,25 @@ export default function WordCountComponent() {
     wordListCountTmp = wordListCountTmp.filter((item: WordListCount) => item.word !== '');
 
     setWordsListCount(wordListCountTmp);
-
+    setTimeout(() => setLoading(false), 1000)
   }, [wordsList]);
 
   return (
-    <div>
-      <textarea onChange={onUserInput} name="text-area" id="text-area"></textarea>
-      <ul>
-        {wordsListCount.map((item: WordListCount, i: number) => (
-          <li key={i}>{item.word} ---- {item.count}</li>
-        ))}
-      </ul>
-      <TableComponent></TableComponent>
+    <div className={`${styles.wordCountComponent} d-flex col-md-10 offset-1 justify-content-evenly mt-5`}>
+      <div className="text-area-container col-md-4">
+        <label htmlFor="floatingTextarea">Type here</label>
+        <textarea rows={Math.round(wordsList.length / 8)} className="form-control" id="floatingTextarea" onChange={onUserInput}></textarea>
+      </div>
+      <div className="words-table col-md-6">
+        {wordsListCount.length && !loading ? (
+          <TableComponent wordsListCount={wordsListCount}></TableComponent>
+        ) :
+          loading ? (
+            <LoadingBox></LoadingBox>
+          ) : (
+            <p>Please start typing for results</p>
+          )}
+      </div>
     </div>
   )
 }

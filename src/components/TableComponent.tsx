e78bfb7ad387929bb3917c7d4a63a686dc7a08f8
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-08 17:00:20 +0200

    ADD, dynamic sorting to table component

--- src/components/TableComponent.tsx
+++ src/components/TableComponent.tsx
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import WordListCount from '../interfaces/interfaces';
+import ByOrder from '../interfaces/interfaces';
 import styles from '../styles/tableComponent.module.css';
 
-const TableComponent = ({ wordsListCount }: WordListCount[] | any) => {
+const TableComponent = ({ wordsListCount, sortData }: any) => {
+
+  const [orderByArray, setOrderByArray] = useState<ByOrder[]>([{ by: 'id', order: 'asc' }, { by: 'word', order: 'asc' }, { by: 'count', order: 'asc' }]);
+  const [orderByArrayTmp, setOrderByArrayTmp] = useState<ByOrder[]>([]);
+
+  const sortDataBy = (orderByTmp: ByOrder): void => {
+    sortData(orderByTmp);
+  }
+
+  const handleClick = (e: any): void => {
+    const by = e.target.getAttribute('id');
+
+    let orderByArrayTmp = orderByArray.map(item => {
+      if (item.by === by) {
+        item.order = item.order === 'asc' ? 'desc' : 'asc';
+      }
+      return item;
+    });
+
+
+
+    const orderBy = orderByArrayTmp.find(item => item.by === by);
+
+    sortDataBy(orderBy!);
+    setOrderByArrayTmp(orderByArrayTmp);
+  }
+
+  useEffect(() => {
+    if (orderByArrayTmp.length)
+      setTimeout(() => setOrderByArray(orderByArrayTmp))
+  }, [orderByArrayTmp])
 
   return (
     <div className={styles.tableComponent}>
       <table className="table">
         <thead>
           <tr>
-            <th scope="col">#</th>
-            <th scope="col">Word</th>
-            <th scope="col">Ocurrence</th>
+            <th scope="col" id="id" onClick={handleClick}>#</th>
+            <th scope="col" id="word" onClick={handleClick}>Word</th>
+            <th scope="col" id="count" onClick={handleClick}>Ocurrence</th>
           </tr>
         </thead>
         <tbody>
           {wordsListCount.map((item: WordListCount, i: number) => (
-            <tr key={i}>
-              <th scope="row">{i + 1}</th>
+            <tr key={item.id}>
+              <th scope="row">{item.id}</th>
               <td>{item.word}</td>
               <td>{item.count}</td>
             </tr>

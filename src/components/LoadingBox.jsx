f068cabf1ace1db7c3adb171f0b9e6e271fce09c
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-08 12:11:11 +0200

    ADD, loading box component

---
+++ src/components/LoadingBox.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function LoadingBox() {
+  return (
+    <div className="">
+      <div class="spinner-grow text-info" role="status">
+      </div>
+    </div>
+  )
+}

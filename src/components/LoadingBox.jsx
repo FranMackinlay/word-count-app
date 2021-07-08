f43a55ffe155910d51aa2a82082ead17976acf5f
Francisco Mackinlay <franmackinlay@gmail.com>
2021-07-08 12:51:40 +0200

    FIX, loading box class

--- src/components/LoadingBox.jsx
+++ src/components/LoadingBox.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function LoadingBox() {
   return (
     <div className="">
-      <div class="spinner-grow text-info" role="status">
+      <div className="spinner-grow text-info" role="status">
       </div>
     </div>
   )
